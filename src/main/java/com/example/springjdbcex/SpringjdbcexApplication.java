909d5a88885ab545bc3f4006e98c1300329f2aaa
Venkatesh Dwivedi <dwivedivenkatesh98@gmail.com>
2025-06-23 17:38:57 +0530

    spring JDBC first commit with all the configurations

---
+++ src/main/java/com/example/springjdbcex/SpringjdbcexApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjdbcex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjdbcexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjdbcexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springjdbcex/SpringjdbcexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjdbcex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjdbcexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
